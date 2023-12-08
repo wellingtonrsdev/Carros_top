ddf3bf6c77ea4e5d51c52e6730c485ebb91a968c
Wellington <rodrigueswellington3@gmail.com>
2023-12-08 16:09:13 -0300

    Project created

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export default function Header() {
+  return (
+    <header>
+      <div className="ct-container">
+        <h1>Carros Top</h1>
+      </div>
+    </header>
+  );
+}
---
+++ src/routes/Catalog/index.tsx
@@ -0,0 +1,8 @@
+import Header from "../../Components/Header";
+
+export default function Catalog() {
+
+    return (
+        <Header />
+    );
+}
